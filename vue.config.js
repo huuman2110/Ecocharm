76dc2efc4f0d91acc7954fb8db0341fc58b32f1d
manth <huuman2110@gmail.com>
2022-05-16 22:54:43 +0700

    production change

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/duanecocharm/'
-    : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
